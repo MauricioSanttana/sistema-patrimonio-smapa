30f6d8595afc779e9adc270753f3c3734d25f579
Maurício Santana <msantana.tech@gmail.com>
2025-10-27 23:00:32 -0300

    Replace old asset data with new entries

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,167 +1,68 @@
+// Dados iniciais do patrimônio SMAPA
 const dadosPatrimonio = [
     {
-        tombamento: "000000165390",
-        descricao: "Computador All In One",
+        tombamento: "000000123456",
+        descricao: "Computador All In One Dell",
         categoria: "Equipamento TI",
-        dataAquisicao: "22/03/2018",
+        dataAquisicao: "15/01/2023",
         setor: "SEC MEIO AMBIENTE",
         responsavel: "Servidor Administrativo",
-        localizacaoEspecifica: "Sala Administrativa",
+        localizacaoEspecifica: "Sala 201",
         situacao: "Ativo",
-        observacoes: "Uso administrativo"
+        observacoes: "Equipamento em perfeito estado",
+        dataInclusao: "27/10/2024"
     },
     {
-        tombamento: "000000119984",
-        descricao: "Impressora Multifuncional",
+        tombamento: "000000123457",
+        descricao: "Impressora Multifuncional HP LaserJet",
         categoria: "Equipamento TI",
-        dataAquisicao: "21/05/2012",
+        dataAquisicao: "20/02/2023",
         setor: "SEC MEIO AMBIENTE",
-        responsavel: "Recepção",
-        localizacaoEspecifica: "Recepção - Balcão",
+        responsavel: "Coordenador Administrativo",
+        localizacaoEspecifica: "Sala 202",
+        situacao: "Ativo",
+        observacoes: "Impressora de uso geral",
+        dataInclusao: "27/10/2024"
+    },
+    {
+        tombamento: "000000123458",
+        descricao: "Veículo Fiat Strada",
+        categoria: "Veículos",
+        dataAquisicao: "10/03/2022",
+        setor: "SEC MEIO AMBIENTE",
+        responsavel: "Motorista Oficial",
+        localizacaoEspecifica: "Garagem Principal",
+        situacao: "Ativo",
+        observacoes: "Placa ABC-1234, para fiscalização",
+        dataInclusao: "27/10/2024"
+    },
+    {
+        tombamento: "000000123459",
+        descricao: "Mesa de Escritório em L",
+        categoria: "Mobiliário",
+        dataAquisicao: "05/05/2023",
+        setor: "SEC MEIO AMBIENTE",
+        responsavel: "Servidor Administrativo",
+        localizacaoEspecifica: "Sala 203",
+        situacao: "Ativo",
+        observacoes: "Mobiliário padrão",
+        dataInclusao: "27/10/2024"
+    },
+    {
+        tombamento: "000000123460",
+        descricao: "Notebook Lenovo ThinkPad",
+        categoria: "Equipamento TI",
+        dataAquisicao: "12/06/2023",
+        setor: "SEC MEIO AMBIENTE",
+        responsavel: "Diretor de Departamento",
+        localizacaoEspecifica: "Sala Direção",
         situacao: "Manutenção",
-        observacoes: "Necessita revisão"
-    },
-    {
-        tombamento: "000000180461",
-        descricao: "Caminhão Iveco Daily 35S14HD",
-        categoria: "Veículos",
-        dataAquisicao: "23/09/2019",
-        setor: "SEC MEIO AMBIENTE",
-        responsavel: "Motorista Setor",
-        localizacaoEspecifica: "Garagem Central",
-        situacao: "Ativo",
-        observacoes: "Veículo oficial, Diesel, Branco"
-    },
-    {
-        tombamento: "000000180462",
-        descricao: "VW Amarok CD 4X4 SE",
-        categoria: "Veículos",
-        dataAquisicao: "23/09/2019",
-        setor: "SEC MEIO AMBIENTE",
-        responsavel: "Diretoria",
-        localizacaoEspecifica: "Garagem Central",
-        situacao: "Ativo",
-        observacoes: "Veículo oficial, Diesel, Branco"
-    },
-    {
-        tombamento: "000000067688",
-        descricao: "Autoclave 134 Litros Sercon",
-        categoria: "Equipamentos Médicos",
-        dataAquisicao: "19/06/2007",
-        setor: "SEC MEIO AMBIENTE",
-        responsavel: "Centro Veterinário",
-        localizacaoEspecifica: "Sala Cirurgia",
-        situacao: "Ativo",
-        observacoes: "Aço inox 304, carro interno"
-    },
-    {
-        tombamento: "000000030705",
-        descricao: "Refrigerador Consul 343L",
-        categoria: "Eletrodomésticos",
-        dataAquisicao: "12/06/2002",
-        setor: "SEC MEIO AMBIENTE",
-        responsavel: "Setor Administrativo",
-        localizacaoEspecifica: "Copa",
-        situacao: "Ativo",
-        observacoes: "220V, necessita revisão"
-    },
-    {
-        tombamento: "000000014766",
-        descricao: "Cadeira Fixa sem Braços",
-        categoria: "Mobiliário",
-        dataAquisicao: "29/07/1988",
-        setor: "SEC MEIO AMBIENTE",
-        responsavel: "Recepção",
-        localizacaoEspecifica: "Sala Espera",
-        situacao: "Ativo",
-        observacoes: "Vinil marrom, Escriba C3"
-    },
-    {
-        tombamento: "000000197575",
-        descricao: "Computador All In One 21,5\"",
-        categoria: "Equipamento TI",
-        dataAquisicao: "01/02/2023",
-        setor: "CENTRO BEM ESTAR ANIMAL",
-        responsavel: "Atendimento",
-        localizacaoEspecifica: "Recepção",
-        situacao: "Ativo",
-        observacoes: "Linha corporativa 2022"
-    },
-    {
-        tombamento: "000000204286",
-        descricao: "Trator Agrícola AB00R 4X4",
-        categoria: "Veículos",
-        dataAquisicao: "16/12/2022",
-        setor: "SEC MEIO AMBIENTE",
-        responsavel: "Operador Equipamentos",
-        localizacaoEspecifica: "Garagem Equipamentos",
-        situacao: "Ativo",
-        observacoes: "Veículo oficial, Diesel, Amarelo"
-    },
-    {
-        tombamento: "000000123292",
-        descricao: "Rádio Comunicação EP450 VHF",
-        categoria: "Comunicação",
-        dataAquisicao: "08/08/2012",
-        setor: "SEC MEIO AMBIENTE",
-        responsavel: "Equipe de Campo",
-        localizacaoEspecifica: "Almoxarifado",
-        situacao: "Ativo",
-        observacoes: "Motorola, 4 unidades"
-    },
-    {
-        tombamento: "000000206025",
-        descricao: "Switch Corporativo L2 24 Portas",
-        categoria: "Equipamento TI",
-        dataAquisicao: "23/02/2024",
-        setor: "SEC MEIO AMBIENTE",
-        responsavel: "TI - Sistema",
-        localizacaoEspecifica: "CPD",
-        situacao: "Ativo",
-        observacoes: "Rede corporativa, 20 portas em uso"
-    },
-    {
-        tombamento: "000000189185",
-        descricao: "Televisor Smart LED 60\"",
-        categoria: "Audiovisual",
-        dataAquisicao: "28/12/2021",
-        setor: "SEC MEIO AMBIENTE",
-        responsavel: "Sala Reuniões",
-        localizacaoEspecifica: "Auditório",
-        situacao: "Ativo",
-        observacoes: "Smart TV, alta definição"
-    },
-    {
-        tombamento: "000000025004",
-        descricao: "Armário Aço 2 Portas",
-        categoria: "Mobiliário",
-        dataAquisicao: "09/04/1999",
-        setor: "SEC MEIO AMBIENTE",
-        responsavel: "Arquivo Geral",
-        localizacaoEspecifica: "Sala Arquivo",
-        situacao: "Ativo",
-        observacoes: "4 prateleiras, anti-ferrugem"
-    },
-    {
-        tombamento: "000000150977",
-        descricao: "Registrador Eletrônico de Ponto",
-        categoria: "Equipamento TI",
-        dataAquisicao: "16/12/2015",
-        setor: "SEC MEIO AMBIENTE",
-        responsavel: "RH - Controle Ponto",
-        localizacaoEspecifica: "Entrada Principal",
-        situacao: "Ativo",
-        observacoes: "Portaria 1.510/2009 MTE"
-    },
-    {
-        tombamento: "000000056773",
-        descricao: "Motocicleta Honda CG150-Job",
-        categoria: "Veículos",
-        dataAquisicao: "24/05/2006",
-        setor: "GABINETE SEC MEIO AMBIENTE",
-        responsavel: "Mensageiro",
-        localizacaoEspecifica: "Garagem",
-        situacao: "Ativo",
-        observacoes: "Veículo oficial, Gasolina, Branca"
+        observacoes: "Em manutenção preventiva",
+        dataInclusao: "27/10/2024"
     }
 ];
+
+// Exportar dados para uso global
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = dadosPatrimonio;
+}
